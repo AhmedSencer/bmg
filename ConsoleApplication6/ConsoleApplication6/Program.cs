aac6001c8f71a6a64a488341b3e7418591aefff3
AhmedSencer <34625556+AhmedSencer@users.noreply.github.com>
2017-12-17 17:49:39 +0300

    Add files via upload

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad; int s, k;
+            Console.WriteLine("Adınızı giriniz:"); ad = Console.ReadLine();
+            s = ad.Length;
+            for (int i = 0; i < s; i++)
+            {
+                k = (int)ad[i] + 10;
+                Console.Write((char)k);
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
